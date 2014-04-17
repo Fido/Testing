2e055c79f710e34b6ffafae70c80da2bb37afdd5
Yonatan <yvalenta@gmail.com>
2014-04-17 18:36:58 -0500

    Se ejecuto migracion

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140406022511) do
+ActiveRecord::Schema.define(version: 20140417223507) do
 
   create_table "personas", force: true do |t|
     t.string   "nombre"
@@ -21,5 +21,16 @@ ActiveRecord::Schema.define(version: 20140406022511) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "posts", force: true do |t|
+    t.string   "titulo"
+    t.text     "contenido"
+    t.string   "extension"
+    t.integer  "persona_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "posts", ["persona_id"], name: "index_posts_on_persona_id", using: :btree
 
 end
